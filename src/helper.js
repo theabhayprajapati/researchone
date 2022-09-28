8fea4eb7038906e4d48ab7f366b3c79f7131c749
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-09-28 12:37:00 +0530

    level 1 id done

--- src/helper.js
+++ src/helper.js
@@ -5,7 +5,37 @@ function getRandomArbitrary(min, max) {
 function generateRandomBoolean() {
     return Math.random() >= 0.5;
 }
+var timer = document.getElementById("timer")
+let seconds;
+let tens;
+function startTimer() {
+    tens++;
+    if (tens <= 9) {
+    }
+    if (tens > 9) {
+    }
+    if (tens > 99) {
+        seconds++;
+        tens = 0;
+    }
+
+    if (seconds > 9) {
+        // appendSeconds.innerHTML = seconds;
+    }
+    timer.innerHTML = seconds + ":" + tens;
+    console.log(seconds + ":" + tens);
+}
+let Interval;
+function start() {
+    tens = 0;
+    seconds = 0;
+    Interval = setInterval(startTimer, 10);
+}
+function stopTimer() {
+    clearInterval(Interval);
+}
+
 export {
-    getRandomArbitrary, generateRandomBoolean
+    getRandomArbitrary, generateRandomBoolean, startTimer, stopTimer, start
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { generateRandomBoolean, getRandomArbitrary } from "./helper.js";
-import { moveLeft, moveRight } from "./movements.js";
+import { generateRandomBoolean, getRandomArbitrary, start, startTimer, stopTimer } from "./helper.js";
 // var mario = document.getElementById('mario');
 var gamefram = document.getElementById("frame");
 var timer = document.getElementById("timer");
 let beforejump;
+var mario;
 function createMario() {
-    var mario = document.createElement("img");
+    mario = document.createElement("img");
     var lefttunnel = document.getElementById("lefttunnel");
     var righttunnel = document.getElementById("righttunel");
     mario.src =
@@ -28,7 +28,7 @@ function createMario() {
     } else {
         randomX = getRandomArbitrary(valueofrightTunnel, valueofrightTunnel);
     }
-    var randomY = getRandomArbitrary(400, 500);
+    var randomY = getRandomArbitrary(0, 0);
     mario.style.left = randomX + "px";
     mario.style.top = randomY + "px";
     mario.style.width = "100px";
@@ -38,120 +38,40 @@ function createMario() {
         window.getComputedStyle(mario).getPropertyValue("top")
     );
 }
-let isJumping = false;
-let isGoingRight = false;
-let isGoingLeft = false;
-let bottom = 0;
-let gravity = 0.9;
-let left = 0;
-let leftTimerId;
-let rightTimerId;
-let seconds = 0;
-let tens = 0;
 let Interval;
-/* when user click on space jump mario */
-document.addEventListener("keydown", function (event) {
 
-    /* space */
-    if (event.keyCode === 83) {
-        isGoingLeft = false;
-        isGoingRight = false;
-        /* if there is mario then destroy it */
-        if (document.getElementById("mario")) {
-            document.getElementById("mario").remove();
+var fallinginterval;
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        clearInterval(fallinginterval);
+        start();
+        const startmessage = document.getElementById("startmessage");
+        if (startmessage) {
+            startmessage.remove()
+        }
+        if (document.getElementById('mario')) {
+            document.getElementById('mario').remove()
         }
         createMario();
-        clearInterval(Interval);
-        tens = 0;
-        seconds = 0;
-        Interval = setInterval(startTimer, 10);
-    }
-});
-/* when user click on right arrow move mario to right */
-document.addEventListener("keydown", function (event) {
-
-    if (event.keyCode === 39) {
-        console.log("Going Right : " + isGoingRight);
-        console.log("Going Left : " + isGoingLeft);
-        isGoingLeft = false;
-        isGoingRight = true;
-        if (isGoingRight) {
-            moveRight();
-        }
-    }
-}); /* when user click on left arrow move mario to left */
-document.addEventListener("keydown", function (event) {
-
-    if (event.keyCode === 37) {
-        console.log("Going Right : " + isGoingRight);
-        console.log("Going Left : " + isGoingLeft);
-        isGoingRight = false;
-        isGoingLeft = true;
-        if (isGoingLeft) {
-            moveLeft();
-        }
-    }
-});
-
-/* on space click jump mario */
-document.addEventListener("keydown", async function (event) {
-
-    if (event.keyCode === 32) {
-        // alert('space');
-        /* if there is mario then destroy it */
-        if (document.getElementById("mario")) {
-            document.getElementById("mario").remove();
-        }
-        createMario();
-        clearInterval(Interval);
-        tens = 0;
-        seconds = 0;
-        Interval = setInterval(startTimer, 10);
-        let yjump = 200;
-
-        var marioscurrentheigth = parseInt(
+        var currentYaxisofmario = parseInt(
             window.getComputedStyle(mario).getPropertyValue("top")
         );
-        // var upinterval = setInterval(function () {
-        //     console.log("jump");
-        //     mario.style.top = marioscurrentheigth - 10 + "px";
-        //     marioscurrentheigth = marioscurrentheigth - 10;
-        //     if (marioscurrentheigth === beforejump - yjump) {
-        //         clearInterval(upinterval);
-
-        //         var downinterval = setInterval(function () {
-        //             console.log("down");
-        //             mario.style.top = marioscurrentheigth + 10 + "px";
-        //             marioscurrentheigth = marioscurrentheigth + 10;
-        //             if (marioscurrentheigth === beforejump) {
-        //                 clearInterval(downinterval);
-        //             }
-        //         }, 10);
-        //     }
-        // }, 10);
-        console.log('going to jump')
-        let gravity = 0.9;
-        var distancebetweenjumpandcurrentheight = beforejump - marioscurrentheigth;
-        // in percentage
-        var percentage = (distancebetweenjumpandcurrentheight / yjump) * 100;
-        while (marioscurrentheigth > yjump) {
-            console.log('jumping')
-            mario.style.top = marioscurrentheigth - (10 * gravity) + "px";
-
-            marioscurrentheigth = parseInt(
-                window.getComputedStyle(mario).getPropertyValue("top")
-            )
-            await sleep()
+        var falling = setInterval(function () {
+            mario.style.top = currentYaxisofmario + 10 + "px";
+            currentYaxisofmario = currentYaxisofmario + 10;
+            if (currentYaxisofmario == 500) {
+                console.log('clear interval');
+                // clear timer
+                // destor mario elemetn
+                mario.remove();
+                clearInterval(falling);
+                console.log(falling)
+            }
+            fallinginterval = falling;
+            console.log(falling)
+            console.log(currentYaxisofmario);
         }
-        while (marioscurrentheigth < beforejump) {
-            console.log('falling')
-            mario.style.top = marioscurrentheigth + (10 * gravity) + "px";
-            marioscurrentheigth = parseInt(
-                window.getComputedStyle(mario).getPropertyValue("top")
-            )
-            await sleep()
-        }
-        mario.style.top = beforejump + "px";
+            , 20);
     }
 });
 const sleep = () => {
@@ -161,30 +81,16 @@ const sleep = () => {
         }, 20)
     })
 }
-function startTimer() {
-    tens++;
-    if (tens <= 9) {
-    }
-    if (tens > 9) {
-    }
-    if (tens > 99) {
-        seconds++;
-        tens = 0;
-    }
 
-    if (seconds > 9) {
-        // appendSeconds.innerHTML = seconds;
+function removemario() {
+    if (mario) {
+        mario.remove();
     }
-    console.log(seconds + ":" + tens);
-    timer.innerHTML = seconds + ":" + tens;
 }
-
-/* onmario click stop timer */
-document.addEventListener("click", function (event) {
-    console.log("click");
-    if (event.target.id === "mario") {
-        clearInterval(Interval);
-        // remove mario
-        document.getElementById("mario").remove();
+document.addEventListener("keydown", function (event) {
+    /* space */
+    if (event.keyCode === 32) {
+        removemario()
+        stopTimer();
     }
-}); 
+})
