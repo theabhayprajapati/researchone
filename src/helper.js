39191006e65037d8c613985f4b67f29ac529e418
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2023-01-20 19:43:35 +0530

    improved performance

--- src/helper.js
+++ src/helper.js
@@ -1,79 +1,232 @@
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function generateRandomBoolean() {
-    return Math.random() >= 0.5;
-}
-var timer = document.getElementById("timer")
-let seconds;
-let tens;
-function startTimer() {
-    tens++;
-    if (tens <= 9) {
-    }
-    if (tens > 9) {
-    }
-    if (tens > 99) {
-        seconds++;
-        tens = 0;
-    }
-
-    if (seconds > 9) {
-        // appendSeconds.innerHTML = seconds;
-    }
-    timer.innerHTML = seconds + ":" + tens;
-}
-let Interval;
-function start() {
-    tens = 0;
-    seconds = 0;
-    Interval = setInterval(startTimer, 10);
-}
-function currenttime() {
-    return seconds + ":" + tens;
-}
-function stopTimer() {
-    clearInterval(Interval);
-}
 
 /* 
-    [
+{
+  "singleTunnel": {
+    "trial": [
+      {
+        "timestamp": 1210,
+        "times": 1
+      },
+      {
+        "timestamp": 167,
+        "times": 2
+      },
+      {
+        "timestamp": 219,
+        "times": 3
+      },
+      {
+        "timestamp": 463,
+        "times": 4
+      },
+      {
+        "timestamp": 67,
+        "times": 10
+      }
+    ],
+    "performance": [
+      {
+        "timestamp": 44,
+        "times": 5
+      },
+      {
+        "timestamp": 42,
+        "times": 6
+      },
+      {
+        "timestamp": 28,
+        "times": 7
+      },
+      {
+        "timestamp": 52,
+        "times": 8
+      },
+      {
+        "timestamp": 37,
+        "times": 9
+      }
+    ]
+  },
+  "doubleTunnel": {
+    "trial": {
+      "center": [
         {
-            mario: 1,
-            timetaken: 0:10
+          "timestamp": 0,
+          "times": 11
+        }
+      ],
+      "left": [
+        {
+          "timestamp": 106,
+          "times": 20
         },
         {
-            mario: 2,
-            timetaken: 0:20
+          "timestamp": 371,
+          "times": 21
         }
-    ]
-*/
-// convert array into csv
-const convertArrayObjectToCSV = (array) => {
-    const columnDelimiter = ',';
-    const lineDelimiter = '\n';
-    const columnnames = [
-        'mario',
-        'timetaken'
-    ]
-    let result = '';
-    result += columnnames.join(columnDelimiter);
-    result += lineDelimiter;
-    array.forEach(item => {
-        /* get the first value of obj item */
-        let firstvalue = Object.values(item)[0].mario;
-        let secondvalue = Object.values(item)[0].timetaken;
-        console.log('firstvalue', firstvalue);
-        console.log('secondvalue', secondvalue);
-        result += firstvalue + columnDelimiter;
-        result += secondvalue + columnDelimiter;
-        result += lineDelimiter;
-    });
-    console.log('result', result);
-    return result;
+      ],
+      "right": [
+        {
+          "timestamp": 0,
+          "times": 12
+        },
+        {
+          "timestamp": 0,
+          "times": 13
+        },
+        {
+          "timestamp": 0,
+          "times": 14
+        },
+        {
+          "timestamp": 101,
+          "times": 22
+        },
+        {
+          "timestamp": 119,
+          "times": 23
+        },
+        {
+          "timestamp": 123,
+          "times": 24
+        }
+      ]
+    },
+    "performance": {
+      "center": [],
+      "left": [
+        {
+          "timestamp": 0,
+          "times": 18
+        },
+        {
+          "timestamp": 70,
+          "times": 27
+        },
+        {
+          "timestamp": 447,
+          "times": 28
+        }
+      ],
+      "right": [
+        {
+          "timestamp": 0,
+          "times": 15
+        },
+        {
+          "timestamp": 0,
+          "times": 16
+        },
+        {
+          "timestamp": 0,
+          "times": 17
+        },
+        {
+          "timestamp": 142,
+          "times": 19
+        },
+        {
+          "timestamp": 182,
+          "times": 25
+        },
+        {
+          "timestamp": 101,
+          "times": 26
+        },
+        {
+          "timestamp": 400,
+          "times": 29
+        }
+      ]
+    }
+  }
+}
 
-};
+
+: CSV
+
+singleTunnel
+trial
+timestamp,times
+1210,1
+167,2
+219,3
+463,4
+67,10
+performance
+timestamp,times
+44,5
+42,6
+28,7
+52,8
+37,9
+doubleTunnel
+trial
+center
+timestamp,times
+0,11
+left
+timestamp,times
+106,20
+371,21
+right
+timestamp,times
+0,12
+0,13
+0,14
+101,22
+119,23
+123,24
+performance
+center
+timestamp,times
+left
+timestamp,times
+0,18
+70,27
+447,28
+right
+timestamp,times
+0,15
+0,16
+0,17
+142,19
+182,25
+101,26
+400,29
+
+
+
+
+*/
+
+function convertArrayObjectToCSV(jsonData) {
+    let csv = "";
+
+    // Iterate through the JSON data
+    for (const tunnelType in jsonData) {
+        csv += tunnelType + "\n";
+        for (const trialType in jsonData[tunnelType]) {
+            csv += ", " + trialType + "\n";
+            if (Array.isArray(jsonData[tunnelType][trialType])) {
+                csv += ", timestamp, times\n";
+                for (const trial of jsonData[tunnelType][trialType]) {
+                    csv += `, ${trial.timestamp}, ${trial.times}\n`;
+                }
+            } else {
+                for (const centerType in jsonData[tunnelType][trialType]) {
+                    csv += ", " + centerType + "\n";
+                    csv += ", timestamp, times\n";
+                    for (const center of jsonData[tunnelType][trialType][centerType]) {
+                        csv += `, ${center.timestamp}, ${center.times}\n`;
+                    }
+                }
+            }
+        }
+    }
+    return csv;
+}
+
 // download csv
 const downloadCSV = (array) => {
     const link = document.createElement('a');
@@ -96,6 +249,6 @@ function filenamefn() {
     return n;
 }
 export {
-    getRandomArbitrary, generateRandomBoolean, startTimer, stopTimer, start, currenttime, downloadCSV
+    downloadCSV
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { downloadCSV } from "./helper";
+
 const HEIGHT = 480;
 const WIDTH = 800;
 const marioHEIGHT = 32;
@@ -13,6 +15,19 @@ canvas.width = WIDTH;
 canvas.style.background = 'black';
 canvas.height = HEIGHT;
 document.body.appendChild(canvas);
+// make a download button below the canvas
+const downloadButton = document.createElement('button');
+downloadButton.innerHTML = 'Download';
+downloadButton.style.position = 'absolute';
+downloadButton.style.bottom = '0';
+downloadButton.style.left = '0';
+downloadButton.setAttribute('id', 'downloadButton');
+downloadButton.addEventListener('click', function () {
+    downloadCSV(game.scores);
+});
+
+document.body.appendChild(downloadButton);
+
 
 class GameRecord {
     constructor(
@@ -20,6 +35,27 @@ class GameRecord {
     ) {
         this.timestamp = 0;
         this.times = 0;
+    }
+}
+const gameValues = {
+    status: {
+        'playing': 'playing',
+        'stop': 'stop',
+        'pause': 'pause',
+        'gameOver': 'gameOver'
+    },
+    modes: {
+        'singleTunnel': 'singleTunnel',
+        'doubleTunnel': 'doubleTunnel',
+    },
+    xPosition: {
+        'left': 'left',
+        'right': 'right',
+        'center': 'center'
+    },
+    modeStatus: {
+        'trial': 'trial',
+        'performance': 'performance'
     }
 }
 class Game {
@@ -33,19 +69,36 @@ class Game {
                 'performance': []
             },
             'doubleTunnel': {
-                'trial': [],
-                'performance': []
+                'trial': {
+                    'center': [],
+                    'left': [],
+                    'right': []
+                },
+                'performance': {
+                    'center': [],
+                    'left': [],
+                    'right': []
+                },
             }
         };
-        this.gameMode = 'singleTunnel';
+        this.gameMode = gameValues.modes.singleTunnel;
         this.times = 0;
+        this.modeStatus = gameValues.modeStatus.trial;
+        this.status = gameValues.status.stop;
     }
     changeGameMode() {
-        if (this.gameMode === 'singleTunnel') {
-            this.gameMode = 'doubleTunnel';
+        if (this.gameMode === gameValues.modes.singleTunnel) {
+            this.gameMode = gameValues.modes.doubleTunnel;
         } else {
-            this.gameMode = 'singleTunnel';
+            this.gameMode = gameValues.modes.singleTunnel;
         }
+    }
+    pause() {
+        this.status = gameValues.status.pause;
+    }
+    pauseUI() {
+        mario.speed = 0;
+        stopwatch.stop();
     }
 }
 
@@ -59,6 +112,7 @@ class Mario {
         this.image.src = 'images/mario.png';
         this.speed = 5;
         this.times = 1;
+        this.xPosition = gameValues.xPosition.center;
     }
     draw() {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -72,13 +126,15 @@ class Mario {
                 // Mario hit the tunnel
                 this.speed = 0;
                 stopwatch.stop();
-                this.onKill();
+                this.onKill('tunnel');
+                this.startOver();
             }
             if (this.y + this.height > HEIGHT) {
                 this.y = HEIGHT - this.height;
                 this.speed = 0;
                 stopwatch.stop();
-                this.onKill();
+                this.onKill('tunnel');
+                this.startOver();
             }
         }
         if (game.gameMode === 'doubleTunnel') {
@@ -86,13 +142,15 @@ class Mario {
                 // Mario hit the tunnel
                 this.speed = 0;
                 stopwatch.stop();
-                this.onKill();
+                this.onKill('tunnel');
+                this.startOver();
             }
             if (this.y + this.height > HEIGHT) {
                 this.y = HEIGHT - this.height;
                 this.speed = 0;
                 stopwatch.stop();
-                this.onKill();
+                this.onKill('tunnel');
+                this.startOver();
             }
         }
     }
@@ -103,8 +161,10 @@ class Mario {
             // random left right 
             const random = Math.floor(Math.random() * 2);
             if (random === 0) {
+                this.xPosition = gameValues.xPosition.left;
                 this.x = RIGHT_CENTER;
             } else {
+                this.xPosition = gameValues.xPosition.right;
                 this.x = LEFT_CENTER;
             }
         }
@@ -117,13 +177,22 @@ class Mario {
         stopwatch.stop();
         stopwatch.start();
     }
-    onKill() {
+    onKill(killedBy) {
         const elapsedTime = stopwatch.getElapsedTime();
-        const record = new GameRecord(elapsedTime, this.times);
-        game.scores[game.gameMode].trial.push(record);
+        const record = {
+            'timestamp': killedBy === 'tunnel' ? 0 : stopwatch.getElapsedTime(),
+            'times': this.times
+        }
+        console.log(record);
+        // game.scores[game.gameMode][game.modeStatus].push(record);
+        if (game.gameMode === gameValues.modes.singleTunnel) {
+            game.scores.singleTunnel[game.modeStatus].push(record);
+        } else {
+            console.log(mario.xPosition)
+            game.scores.doubleTunnel[game.modeStatus][mario.xPosition].push(record);
+        }
+        console.log(game.scores);
         this.times++;
-        this.startOver();
-
     }
     position(x) {
         this.x = x;
@@ -198,27 +267,54 @@ document.addEventListener('keyup', (event) => {
 });
 // right arrow key
 document.addEventListener('keydown', (event) => {
-    if (event.code === 'ArrowRight') {
-        mario.x += 5;
+    if (game.status === gameValues.status.playing) {
+        if (game.gameMode === gameValues.modes.doubleTunnel) {
+            if (mario.xPosition === gameValues.xPosition.right) {
+                if (event.code === 'ArrowRight') {
+                    track();
+                }
+            }
+        }
     }
 });
 // left arrow key
 document.addEventListener('keydown', (event) => {
-    if (event.code === 'ArrowLeft') {
-        mario.x -= 5;
+    if (game.status === gameValues.status.playing) {
+        if (game.gameMode === gameValues.modes.doubleTunnel) {
+            if (mario.xPosition === gameValues.xPosition.left) {
+                if (event.code === 'ArrowLeft') {
+                    track();
+                }
+            }
+        }
     }
 });
 /* space speed 0 */
 document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        mario.speed = 0;
-        stopwatch.stop();
+    if (game.status === gameValues.status.playing) {
+        if (game.gameMode === gameValues.modes.singleTunnel) {
+            if (event.code === 'Space') {
+                track();
+            }
+        }
     }
 });
+function track() {
+    game.pauseUI();
+    /* pause for random time */
+    const random = Math.floor(Math.random() * 1000);
+    console.log(game.scores);
+    mario.onKill();
+    // setTimeout(() => {
+    mario.startOver();
+    // }, random * 10);
+}
 // start over click r
 document.addEventListener('keydown', (event) => {
     if (event.code === 'KeyR') {
         mario.startOver();
+        game.status = gameValues.status.playing;
+        game.gameMode = gameValues.modes.singleTunnel;
     }
 });
 /* click m change game mode */
@@ -229,6 +325,13 @@ document.addEventListener('keydown', (event) => {
         mario.startOver();
     }
 });
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'KeyS') {
+        game.status = gameValues.status.playing;
+        console.log(game.status);
+    }
+}
+);
 stopwatch.start();
 const game = new Game();
 class DoubleTunnel {
@@ -252,25 +355,85 @@ class DoubleTunnel {
 
 const doubleTunnel = new DoubleTunnel();
 
+function repharseGame(game, mario) {
+    if (mario.times >= 0 && mario.times <= 10) {
+        game.gameMode = 'singleTunnel'
+    } else if (mario.times >= 10 && mario.times <= 30) {
+        game.gameMode = 'doubleTunnel'
+    }
+}
 
 function draw() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    mario.draw();
-    mario.move();
-    mario.checkCollision();
-    if (game.gameMode === 'singleTunnel') {
-        tunnel.draw();
+    if (game.status == gameValues.status.stop) {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        ctx.fillText(`Press S to start`, 20, 40);
+    } else if (game.status == 'playing') {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        mario.draw();
+        mario.move();
+        mario.checkCollision();
+        if (game.gameMode === gameValues.modes.singleTunnel) {
+            tunnel.draw();
+        } else {
+            doubleTunnel.draw();
+        }
+        repharseGame(game, mario);
+        if (mario.times >= 30) {
+            game.status = gameValues.status.gameOver;
+        }
+        // Display elapsed time
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        ctx.fillText(`Elapsed Time: ${stopwatch.getElapsedTime()}ms`, 20, 40);
+        // Display times
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        /* 
+        if marios times
+        */
+        ctx.fillText(`Times: ${mario.times}`, 20, 80);
+        takeMarioTimeGiveStatusMessages(mario);
+    } else if (game.status == gameValues.status.gameOver) {
+        stopwatch.stop();
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        ctx.fillText(`Game Over`, 20, 40);
+        ctx.fillText(`Press R to restart`, 20, 80);
+        ctx.fillText(`Press M to change game mode`, 20, 120);
+        ctx.fillText(`Your score: ${mario.times}`, 20, 160);
+        ctx.fillText(`Your time: ${stopwatch.getElapsedTime()}ms`, 20, 200);
+        ctx.fillText(`Your game mode: ${game.gameMode}`, 20, 240);
+        ctx.fillText(`Your status: ${statusMessages[mario.times]}`, 20, 280);
     } else {
-        doubleTunnel.draw();
+        console.log("inside else...")
     }
-    // Display elapsed time
-    ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
-    ctx.fillText(`Elapsed Time: ${stopwatch.getElapsedTime()}ms`, 20, 40);
-    // Display times
-    ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
-    ctx.fillText(`Times: ${mario.times}`, 20, 80);
     requestAnimationFrame(draw);
 }
 draw();
+
+var statusMessages = {
+    '1': 'trial SINGLE HAND',
+    '2': 'Performance SINGLE HAND',
+    '3': 'Trial DOUBLE HAND LEFT',
+    '4': 'Performance DOUBLE HAND LEFT',
+    '5': 'Trial DOUBLE HAND RIGHT',
+    '6': 'Performance DOUBLE HAND RIGHT',
+}
+function takeMarioTimeGiveStatusMessages(mario) {
+    // divide by 5
+    var times = Math.floor(mario.times / 5);
+    console.log(times);
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    ctx.fillText(`Your status: ${statusMessages[(times + 1)]}`, (WIDTH - ((50 * WIDTH) / 100)), 40);
+    var modeStatus = statusMessages[(times + 1)]?.toLowerCase().split(' ');
+    console.log(modeStatus);
+    if (modeStatus.includes('trial')) {
+        game.modeStatus = gameValues.modeStatus.trial;
+    } else {
+        game.modeStatus = gameValues.modeStatus.performance;
+    }
+}
